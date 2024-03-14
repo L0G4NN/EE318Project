2f52082b9a2132d10ebe49f2d0072b56b3cc5625
Daniel342004 <126585093+Daniel342004@users.noreply.github.com>
2024-03-14 11:50:09 +0000

    Add files via upload

---
+++ actuator.h
@@ -0,0 +1,15 @@
+/*
+* actuator.h
+*
+*/
+#ifndef __ACTUATOR_H__
+#define __ACTUATOR_H__
+
+#include <msp430.h>
+
+void initialise_actuator()
+void drop_tree()
+void turn_forward()
+
+
+#endif // __ACTUATOR_H__
