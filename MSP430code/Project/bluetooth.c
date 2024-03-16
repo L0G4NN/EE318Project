d872e19b25b56f8d4b5392bd918542d09c886403
Caileano78 <Caileano78@gmail.com>
2024-03-16 20:59:18 +0000

    playing around

--- MSP430code/Project/bluetooth.c
+++ MSP430code/Project/bluetooth.c
@@ -5,20 +5,22 @@
 #define LED BIT0
 
 void bluetooth_init() {
-    // Initialize UART
+    // Initialise UART
     P1SEL0 |= RXD; // Set RXD pin to UART mode
 
+
     // Configure LED pin
-    P1DIR |= LED; // Set LED pin as output
-    P1OUT &= ~LED; // Turn off LED
+    P4DIR |= LED; // Set LED pin as output
+    P4OUT &= ~LED; // Turn off LED
 
     // Configure UART settings
     UCA0CTLW0 |= UCSWRST; // Put eUSCI in reset
     UCA0CTLW0 |= UCSSEL__SMCLK; // Select SMCLK as clock source
     UCA0BRW = 52; // Set baud rate to 9600 (assuming 1 MHz SMCLK)
     UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900; // Modulation UCBRSx = 0x49, UCBRFx = 1
-    UCA0CTLW0 &= ~UCSWRST; // Initialize eUSCI
+    UCA0CTLW0 &= ~UCSWRST; // Initialise eUSCI
     UCA0IE |= UCRXIE; // Enable USCI_A0 RX interrupt
+
 }
 
 void bluetooth_check() {
@@ -28,10 +30,10 @@ void bluetooth_check() {
         // Check received character and control LED accordingly
         switch (received_char) {
             case '1':
-                P1OUT |= LED; // Turn on LED
+                P4OUT |= LED; // Turn on LED
                 break;
             case '0':
-                P1OUT &= ~LED; // Turn off LED
+                P4OUT &= ~LED; // Turn off LED
                 break;
             default:
                 break;
