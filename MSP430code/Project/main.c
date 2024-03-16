131714a7287d26c2590c3ac68f6e3e18d221dd6a
Logan <logannoonan@outlook.com>
2024-03-16 12:32:23 +0000

    testing

--- MSP430code/Project/main.c
+++ MSP430code/Project/main.c
@@ -1,25 +1,25 @@
 /*
- * main.c
- * EE318 Group 24
+ *  main.c
+ *  EE318 Group 24
  *
- * TODO:
- * Implement interrupts
- * Implement bluetooth
- * Implement PWM of motors
- * Implement actuation control
+ *  TODO:
+ *  Implement interrupts
+ *  Implement bluetooth
+ *  Implement PWM of motors
+ *  Implement actuation control
  *
  */
 
 
 /*
-* MSP430 PIN CONNECTIONS
-* BLUETOOTH: PORT 1 PINS 0 TX,1 RX
-* MOTOR: PORT X PINS Y
-* SERVO: PORT X PINS Y
+*   MSP430 PINOUT
+*   BLUETOOTH: PORT 1 PINS 0 TX,1 RX
+*   MOTOR: PORT 1 PINS 5,6,7 AND PORT 5 PIN 0
+*   SERVO: PORT X PINS Y
 */
 
 #include <msp430.h>
-#include <driverlib.h> //unsure if it is needed whilst using CCS
+#include <driverlib.h>  //unsure if it is needed whilst using CCS
 #include "motor.h"
 #include "bluetooth.h"
 
@@ -27,7 +27,7 @@
 unsigned char SW1_interruptFlag = 0;
 
 #pragma vector = PORT1_VECTOR
-__interrupt void P1_ISR(void) //interrupts on PORT1
+__interrupt void P1_ISR(void)   //interrupts on PORT1
 {
 	switch (__even_in_range(P1IV, P1IV_P1IFG7))
 	{
@@ -38,14 +38,20 @@ __interrupt void P1_ISR(void) //interrupts on PORT1
 	}
 }
 
-//ISR for CCR0 and CCR1 capture compare registeres - to set PWM
+char signal = 'w';   //for testing purposes -- will be set by bluetooth in practice
+
+//ISR for CCR0 and CCR1 capture compare registers
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void ISR_TA0_CCR0(void)
 {
-    P4OUT |= BIT0; //SET LED HIGH
+    P4OUT |= BIT0;  //SET LED HIGH
     P8OUT |= BIT0;
-    TA0CCTL0 &= ~CCIFG; //clear interrupt flag
 
+    P1OUT |= BIT7;
+
+    //drive(signal);
+
+    TA0CCTL0 &= ~CCIFG; //clear interrupt flag
 }
 
 #pragma vector = TIMER0_A1_VECTOR
@@ -53,33 +59,35 @@ __interrupt void ISR_TA0_CCR1(void)
 {
     P4OUT &= ~BIT0; //SET LED LOW
     P8OUT &= ~BIT0;
-    TA0CCTL1 %= ~CCIFG; //clear interrupt
 
+    P1OUT &= ~BIT7;
+
+    //drive(signal);
+
+    TA0CCTL1 %= ~CCIFG; //clear interrupt
 }
 
-//interrupt handler for the timer
-//see user guide 11.2.6 and msp430fr4133 line 3016
 
 void main(void)
 {
-	//Default MCLK = 1MHz and SMCLK = MCLK
 
-    WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;   //stop watchdog timer
 	PM5CTL0 &= ~LOCKLPM5;
     __enable_interrupt();
+    //__bis_SR_register(LPM0_bits);
 
-	//initialise timers
+	//initialise motor DO and timers
+    initMotors();
 	initPWMTimers();
 
-	bluetooth_init();
+	//bluetooth_init();
 
 	//MAIN PROGRAM LOOP
 	while(1)
 	{
 	    //_lab_test_();
 	    //delay_us(1);
-
-	    bluetooth_check();
+	    //bluetooth_check();
 	}
 }
 
--- MSP430code/Project/motor.c
+++ MSP430code/Project/motor.c
@@ -1,19 +1,19 @@
 /******************
-*motor.c
+* motor.c
 *
-*Implementation of controlling the
-*motors on our vehicle
+* Implementation of controlling the
+* motors on our vehicle
 *
-*Logan Noonan
-*Daniel Ofem
-*TODO:
-*Finish Drive()
-*Finish Timers function
-*Test on hardware
+* Logan Noonan
 *
-*https://dev.ti.com/tirex/explore/node?node=A__AMqhsgBhm3-jH1WI3FE0eA__msp430ware__IOGqZri__LATEST
-*Brock J. LaMers - Embedded Systems Design
-*http://www.msp430launchpad.com/2010/07/timers-and-clocks-and-pwm-oh-my.html
+* TODO:
+* Finish Drive()
+* Test on hardware
+*
+* Useful references
+* https://dev.ti.com/tirex/explore/node?node=A__AMqhsgBhm3-jH1WI3FE0eA__msp430ware__IOGqZri__LATEST
+* Brock J. LaMers - Embedded Systems Design
+* http://www.msp430launchpad.com/2010/07/timers-and-clocks-and-pwm-oh-my.html
 ******************/
 
 #include "motor.h"
@@ -22,84 +22,54 @@
 //Set the output pins for ENABLE1&2, and inputs 1A,2A,3A,4A of the motor.
 void initMotors()
 {
+    //MOTOR PORTS
+    /*
+     * Motor A - P1.7, P1.6 -- Left hand side
+     * Motor B - P1.5, P5.0 -- Right hand side
+     */
+
+    P1DIR |= BIT7;
+    P1DIR |= BIT6;
+    P1DIR |= BIT5;
+    P5DIR |= BIT0;
+
+    //LED VISUALISATION PORTS
+    P2DIR |= BIT7;
+    P2OUT &= ~BIT7;  //set 2.7 cathode LOW
+
+    P8DIR |= BIT0;  //LED1
+    P5DIR |= BIT1;  //LED2
+    P5DIR |= BIT2;  //LED3
+    P5DIR |= BIT4;  //LED4
 }
-
-/*
- * void initTimers()
-{
-
-    //TODO: CAN SET THE PWM SIGNAL ONTO PORT 4 PIN 0 - ONBOARD LED FINE. NEED TO SETUP PWM ONTO THE OUTPUT PINS AND MEASURE RESPONSE IN THE LAB
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P4, GPIO_PIN0, GPIO_PRIMARY_MODULE_FUNCTION);
-    //GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
-
-    int DUTY_CYCLE = 850; //In OUTPUTMODE_SET_RESET the duty cycle value is the value of "downtime" in the PWM signal.
-    int TIMER_A_PERIOD = 1000;
-
-    //TODO: FINISH FUNCTION -
-    // HAVE A TOTAL OF 4 PWM SIGNALS RUNNING AT THE ONE TIME - WHERE 2 ARE ALWAYS CONSTANT LOW(CONSTANT LOW UNTIL THEY ARE NEEDED FOR REVERSE)
-    // HAVE THE 2 PWM SIGNALS PULSING AT THE SAME RATE UNTIL A DIRECTIONAL CHANGE OCCURS
-
-    Timer_A_outputPWMParam param = {0};
-    param.clockSource = TIMER_A_CLOCKSOURCE_ACLK;
-    param.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_32;
-    param.timerPeriod = TIMER_A_PERIOD;
-    param.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_1; //ERROR MAYBE HERE?
-    param.compareOutputMode = TIMER_A_OUTPUTMODE_SET_RESET;
-    param.dutyCycle = DUTY_CYCLE;
-
-    Timer_A_outputPWM(TIMER_A1_BASE, &param);
-
-    __bis_SR_register(LPM0_bits); //accidently deleted this and broke everything
-}
- *
- */
 
 void initPWMTimers()
 {
     //SETUP PORTS
     P4DIR |= BIT0;
-    P4OUT |= BIT0; //P4.0 LED initially set to HIGH
+    P4OUT |= BIT0;  //P4.0 LED initially set to HIGH
+    P1OUT |= BIT7;
 
     //Setup timer control registers
-    TA0CTL |= TACLR; // -- clear the timer to init
+    //TA0CTL |= TACLR;    // -- clear the timer to init
     TA0CTL |= TASSEL__ACLK; //init ACLK 32.768kHz
-    //TA0CTL |= ID__8; //Divide ACLK by 8 - 4.096kHz
-    TA0CTL |= MC__UP; //Set UP mode
+    TA0CTL |= ID__8;  //Divide ACLK by 8 - 4.096kHz
+    TA0CTL |= MC__UP;   //Set UP mode
 
     //Capture compare registers
-    TA0CCR0 = 33; // delta t = T * N - set PWM period
-    TA0CCR1 = 16;
+    //TODO: FIGURE OUT APPROPRIATE TIMING VALUES
+    TA0CCR0 = 65535;   //delta t = T * N - set PWM period
+    TA0CCR1 = 2054;
 
-    //Enable interrupts on capture compare
-
-    //TA0CCTL0 |= CM_1; //Rising edge
-    //TA0CCTL0 |= CCIS_1; //compare to the value stored in CCR1
-    //TA0CCTL0 |= OUTMOD_7;
-    TA0CCTL0 |= CCIE; //local enable for CCR0
-    TA0CCTL1 |= CCIE;
+    TA0CCTL0 |= CM_1; //Rising edge
+    TA0CCTL0 |= CCIS_1;   //compare to the value stored in CCR1
+    TA0CCTL0 |= OUTMOD_6;
+//    TA0CCTL0 |= CCIE;   //local interrupt enable for CCR0
+//    TA0CCTL1 |= CCIE;
 
     //clear any interrupts
     TA0CCTL0 &= ~CCIFG;
     TA0CCTL1 &= ~CCIFG;
-
-    //__enable_interrupt();
-
-    //MOTOR PORTS
-
-    //LED VISUALISATION PORTS
-    P2DIR |= BIT7;
-    P2OUT &= ~BIT7; //set 2.7 cathode low
-
-    P8DIR |= BIT0; //LED 1
-    //P8OUT |= BIT0;
-    P5DIR |= BIT1; //LED 2
-    //P5OUT |= BIT2;
-    P5DIR |= BIT2; //LED 3
-    //P5DIR |= BIT2;
-    P5DIR |= BIT4; //LED 4
-    //P5OUT |= BIT4;
-
-
 }
 
 void delay_us(int microseconds)
@@ -119,19 +89,46 @@ void _lab_test_()
 
 void drive(char signal)
 {
-    //TODO: FINISH FUNCTION NOW PWM WORKS
-    if(signal == 'w') //drive forwards
-    {
-        initPWMTimers();
-    }
-    else if(signal == 'a') //turn left
-    {
-    }
-    else if(signal == 'd') //turn right
-    {
-    }
-    else if(signal == 's') //backwards
-    {
+    //TODO: FINISH FUNCTION NOW PWM WORKS -- THIS DOES NOT WORK AS EXPECTED
+    switch (signal) {
+        case 'w':
+            P1OUT ^= BIT7;  //Toggle PIN
+            P1OUT ^= BIT5;
+
+            //P8OUT ^= BIT0;  //LED PINS
+            //P5OUT ^= BIT2;
+            break;
+        case 'a':
+            //Toggle RIGHT motor, LEFT motor stationary -- maybe backwards?
+            P1OUT &= ~BIT7;
+            //P1OUT ^= BIT6;
+            P1OUT ^= BIT5;
+            break;
+
+        case 'd':
+            //Toggle LEFT motor, RIGHT motor statinary -- maybe backwards?
+            P1OUT ^= BIT7;
+            P1OUT &= ~BIT5;
+            //P5OUT ^= BIT0;
+            break;
+
+        case 's':
+            //Toggle backwards pins
+            P1OUT ^= BIT6;
+            P5OUT ^= BIT0;
+
+            P5OUT ^= BIT1;
+            P5OUT ^= BIT3;
+            break;
+
+        default:
+            //Stay still -- all pins written to LOW
+            P1OUT &= ~BIT7;
+            P1OUT &= ~BIT6;
+            P1OUT &= ~BIT5;
+            P5OUT &= ~BIT0;
+            break;
     }
 }
+
 
