9d1709de0ed763969e9952ce0203001e0d57f538
Logan <logannoonan@outlook.com>
2024-03-18 19:01:33 +0000

    Update actuator.c

--- MSP430code/Project/actuator.c
+++ MSP430code/Project/actuator.c
@@ -1,6 +1,11 @@
 /*
 * actuator.c
 *
+*
+*   -- Might be easier if I give you PIN1.7 due to its built-in PWM functionality (Digital IO section in the userguide p314/319)
+*      P1SEL1
+*
+*      Should be able to control PWM with TIMERA1 and only the pin itself, no need for CAPTURE/COMPARE registers
 */
 
 #include "actuator.h"
@@ -19,6 +24,5 @@ void turn_forward(){
     TA1CCTL0 |= ID__8;
     TA1CCTL0 |= TACLR;
 
-    
 }
 
